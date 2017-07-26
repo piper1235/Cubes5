101c3490c7289042a08f8fd053fa07f3bb0d0f83
Tony <piper1235@yahoo.com>
2017-07-26 16:05:33 -0700

    9 - CoreData again  -  First commit before i fuck it up again

---
+++ Cubes5/GameViewController.swift
@@ -0,0 +1,48 @@
+//
+//  GameViewController.swift
+//  Cubes5
+//
+//  Created by Tony on 7/26/17.
+//  Copyright © 2017 Anthony Vitrano. All rights reserved.
+//
+
+import UIKit
+
+class GameViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var gameImageView: UIImageView!
+    
+    var imagePicker = UIImagePickerController()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        imagePicker.delegate = self
+    }
+
+
+    @IBAction func photosTapped(_ sender: Any) {
+        
+        imagePicker.sourceType = .photoLibrary
+        
+        present(imagePicker, animated: true, completion: nil)
+        
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        let image = info[UIImagePickerControllerOriginalImage] as! UIImage
+        
+        gameImageView.image = image
+        
+        imagePicker.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func cameraTapped(_ sender: Any) {
+    }
+    
+    
+    @IBAction func addTapped(_ sender: Any) {
+    }
+    
+}
